ac28fcf79356223ac01e152c8b4d6ea91bccbe7f
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-01-11 14:29:53 -0500

    Remove taxonomy-top-list and rename search-preprint component to search-retraction-watch.

--- tests/integration/components/search-retraction-watch-test.js
+++ tests/integration/components/search-retraction-watch-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('search-preprints', 'Integration | Component | search preprints', {
+moduleForComponent('search-retraction-watch', 'Integration | Component | search preprints', {
   integration: true
 });
 
@@ -9,7 +9,7 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{search-preprints}}`);
+  this.render(hbs`{{search-retraction-watch}}`);
 
   assert.equal(this.$('button').text(), 'Search');
 });
