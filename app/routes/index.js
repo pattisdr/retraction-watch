cd4bba52809c164883e3cbe2cc5e5ef52cc63c94
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-01-12 11:49:43 -0500

    Remove model from index route.

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,29 +4,6 @@ import ResetScrollMixin from '../mixins/reset-scroll';
 import Analytics from '../mixins/analytics';
 
 export default Ember.Route.extend(Analytics, ResetScrollMixin, {
-    // store: Ember.inject.service(),
-    theme: Ember.inject.service(),
-    model() {
-        return Ember.RSVP.hash({
-            taxonomies: this.get('theme.provider')
-                .then(provider => provider
-                    .query('taxonomies', {
-                        filter: {
-                            parents: 'null'
-                        },
-                        page: {
-                            size: 20
-                        }
-                    })
-                ),
-            brandedProviders: this
-                .store
-                .findAll('preprint-provider', { reload: true })
-                .then(result => result
-                    .filter(item => item.id !== 'osf')
-                )
-        });
-    },
     actions: {
         search(q) {
             let route = 'discover';
