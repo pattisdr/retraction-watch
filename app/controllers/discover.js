89ec29d20b78efbc213f275d35e2cad93e74b9f3
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-01-20 11:55:57 -0500

    Override panel-heading for facets.

--- app/controllers/discover.js
+++ app/controllers/discover.js
@@ -1,13 +1,9 @@
 import Ember from 'ember';
 import Analytics from '../mixins/analytics';
 
-// KEEP
 let filterQueryParams = ['tags', 'sources', 'publishers', 'funders', 'institutions', 'organizations', 'language', 'contributors', 'type'];
 
 export default Ember.Controller.extend(Analytics, {
-    theme: Ember.inject.service(), // jshint ignore:line
-    // TODO: either remove or add functionality to info icon on "Refine your search panel"
-
     queryParams:  Ember.computed(function() {
         let allParams = ['q', 'start', 'end', 'sort', 'page'];
         allParams.push(...filterQueryParams);
